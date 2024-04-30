4d167d704990864f8bb79db8b7792731da0321c9
Kusal KRP <kusalcoc1212@gmail.com>
2024-04-30 12:17:01 +0530

    update configdev

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,5 +1,5 @@
 window.config = {
-    apiUrl: 'https://d3a34988-bf91-42db-a5dc-056dd611b04f-dev.e1-us-cdp-2.choreoapis.dev/sycodoca/balapi/bal-api-d0e/v1.0',  
+    apiUrl: 'https://d3a34988-bf91-42db-a5dc-056dd611b04f-dev.e1-us-cdp-2.choreoapis.dev/sycodoca/balapi/bal-api-d0e/v1',  
     auth:{
         signInRedirectURL: "http://localhost:5173",
 		signOutRedirectURL: "http://localhost:5173",
