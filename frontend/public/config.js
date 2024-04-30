2cb9ff857a44df8e7f63dcd45c49ca7b4fd3e8a4
Kusal KRP <kusalcoc1212@gmail.com>
2024-04-30 11:17:56 +0530

    create config

---
+++ frontend/public/config.js
@@ -0,0 +1,10 @@
+window.configs = {
+    apiUrl: '/choreo-apis/sycodoca/balapi/bal-api-d0e/v1.0',  
+    auth:{
+        signInRedirectURL: "http://localhost:5173",
+		signOutRedirectURL: "http://localhost:5173",
+		clientID: "M4qEzHtcHPrd8fr8qvgP4tuYKR0a",
+		baseUrl: "https://api.asgardeo.io/t/diyasen",
+		scope: [ "openid","profile" ]
+    }      
+};
