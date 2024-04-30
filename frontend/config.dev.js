12cd81e9685a6dbdce1a08f763537ca10bc48a68
Kusal KRP <kusalcoc1212@gmail.com>
2024-04-30 11:59:29 +0530

    updatenew config

--- frontend/config.dev.js
+++ frontend/config.dev.js
@@ -1,8 +1,8 @@
 window.config = {
     apiUrl: 'https://d3a34988-bf91-42db-a5dc-056dd611b04f-dev.e1-us-cdp-2.choreoapis.dev/sycodoca/balapi/bal-api-d0e/v1.0',  
     auth:{
-        signInRedirectURL: "https://3bfb4f24-d2c2-4edf-84fb-2b6a0bdce305.e1-us-cdp-2.choreoapps.dev ",
-		signOutRedirectURL: "https://3bfb4f24-d2c2-4edf-84fb-2b6a0bdce305.e1-us-cdp-2.choreoapps.dev ",
+        signInRedirectURL: "https://3bfb4f24-d2c2-4edf-84fb-2b6a0bdce305.e1-us-cdp-2.choreoapps.dev",
+		signOutRedirectURL: "https://3bfb4f24-d2c2-4edf-84fb-2b6a0bdce305.e1-us-cdp-2.choreoapps.dev",
 		clientID: "M4qEzHtcHPrd8fr8qvgP4tuYKR0a",
 		baseUrl: "https://api.asgardeo.io/t/diyasen",
     }      
