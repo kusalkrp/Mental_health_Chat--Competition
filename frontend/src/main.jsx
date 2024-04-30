3587d72b7f8078d925f22a7acf4965cee2a39077
Kusal KRP <kusalcoc1212@gmail.com>
2024-04-30 12:31:15 +0530

    upmsin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,11 +8,11 @@ import { AuthProvider } from '@asgardeo/auth-react';
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<AuthProvider
     config={{
-		signInRedirectURL: "http://localhost:5173",
-		signOutRedirectURL: "http://localhost:5173",
-		clientID: "M4qEzHtcHPrd8fr8qvgP4tuYKR0a",
-		baseUrl: "https://api.asgardeo.io/t/diyasen",
-		scope: [ "openid","profile" ]
+      signInRedirectURL: "https://3bfb4f24-d2c2-4edf-84fb-2b6a0bdce305.e1-us-cdp-2.choreoapps.dev",
+      signOutRedirectURL: "https://3bfb4f24-d2c2-4edf-84fb-2b6a0bdce305.e1-us-cdp-2.choreoapps.dev",
+      clientID: "M4qEzHtcHPrd8fr8qvgP4tuYKR0a",
+      baseUrl: "https://api.asgardeo.io/t/diyasen",
+      scope: [ "openid","profile" ]
     }}
   >
     <ContextProvider>
